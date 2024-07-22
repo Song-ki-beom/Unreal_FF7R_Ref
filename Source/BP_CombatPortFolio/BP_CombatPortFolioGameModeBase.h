e124ae518c71ea673de7b1a9e598ca935268b48d
kiboem <63495505+Song-ki-beom@users.noreply.github.com>
2024-07-22 18:50:53 +0900

    content 제외 후 커밋

---
+++ Source/BP_CombatPortFolio/BP_CombatPortFolioGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BP_CombatPortFolioGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BP_COMBATPORTFOLIO_API ABP_CombatPortFolioGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
